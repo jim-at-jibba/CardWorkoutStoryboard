b30c5897210b9f01457906191817ba63d7b6e972
James Best <james.best@breedr.co>
2023-06-23 13:41:58 +0100

    Add logic for switching images and buttons

--- CardWWorkout/CardSelectionVC.swift
+++ CardWWorkout/CardSelectionVC.swift
@@ -10,17 +10,19 @@ import UIKit
 class CardSelectionVV: UIViewController {
     //@IBOutlet Give access to the object in code
     @IBOutlet var CardImageView: UIImageView!
+    @IBOutlet var Buttons: [UIButton]!
     
     // not needed as we now have outlet collection
-//    @IBOutlet var restartButton: UIButton!
-//    @IBOutlet var rulesButton: UIButton!
-//    @IBOutlet var stopButton: UIButton!
+    // @IBOutlet var restartButton: UIButton!
+    // @IBOutlet var rulesButton: UIButton!
+    // @IBOutlet var stopButton: UIButton!
     
-    @IBOutlet var Buttons: [UIButton]!
+    var timer: Timer!
+    var cards: [UIImage] = Deck.allValues
     
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        startTimer()
         // border radius can only be set programically
         // This is quite repetative. Use OutletCollection instead
         // stopButton.layer.cornerRadius = 8
@@ -31,13 +33,26 @@ class CardSelectionVV: UIViewController {
         }
     }
     
+    override func viewDidDisappear(_ animated: Bool) {
+        // If you dont call this, you wont get all the functionality that apple has built for you
+        super.viewWillDisappear(animated)
+        timer.invalidate()
+    }
+   
+    func startTimer() {
+        timer = Timer.scheduledTimer(timeInterval: 0.1, target: self, selector: #selector(showRandomImage), userInfo: nil, repeats: true)
+    }
+    
+    @objc func showRandomImage() {
+        CardImageView.image = cards.randomElement() ?? UIImage(named: "AS")
+    }
     //@IBAction allows you to do something
     @IBAction func restartButtonTapped(_ sender: UIButton) {
+        timer.invalidate()
+        startTimer()
     }
     
     @IBAction func stopButtonTapped(_ sender: UIButton) {
-    }
-    
-    @IBAction func rulesButtonTapped(_ sender: UIButton) {
+        timer.invalidate()
     }
 }
