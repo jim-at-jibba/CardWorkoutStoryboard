149ffd6823afc8abce3eeb00a218f3ef7103a23e
James Best <james.best@breedr.co>
2023-06-23 13:18:07 +0100

    UI and modal screen fisihed

--- CardWWorkout/CardSelectionVC.swift
+++ CardWWorkout/CardSelectionVC.swift
@@ -8,22 +8,36 @@
 import UIKit
 
 class CardSelectionVV: UIViewController {
-
+    //@IBOutlet Give access to the object in code
+    @IBOutlet var CardImageView: UIImageView!
+    
+    // not needed as we now have outlet collection
+//    @IBOutlet var restartButton: UIButton!
+//    @IBOutlet var rulesButton: UIButton!
+//    @IBOutlet var stopButton: UIButton!
+    
+    @IBOutlet var Buttons: [UIButton]!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
+        // border radius can only be set programically
+        // This is quite repetative. Use OutletCollection instead
+        // stopButton.layer.cornerRadius = 8
+        // restartButton.layer.cornerRadius = 8
+        // rulesButton.layer.cornerRadius = 8
+        for button in Buttons {
+            button.layer.cornerRadius = 8
+        }
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    //@IBAction allows you to do something
+    @IBAction func restartButtonTapped(_ sender: UIButton) {
     }
-    */
-
+    
+    @IBAction func stopButtonTapped(_ sender: UIButton) {
+    }
+    
+    @IBAction func rulesButtonTapped(_ sender: UIButton) {
+    }
 }
